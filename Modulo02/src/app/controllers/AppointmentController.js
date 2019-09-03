b0eaeffde0e62e16509809959c837f836f5732e8
TiagoBehenck <tiago.behenck.santos@gmail.com>
2019-09-03 17:25:42 -0300

    Setting up queue

--- Modulo02/src/app/controllers/AppointmentController.js
+++ Modulo02/src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancelletionMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -133,17 +134,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancelletionMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
