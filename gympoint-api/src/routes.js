fca166bf7b186796c383c12fe18a86240d9ade47
TiagoBehenck <tiago.behenck.santos@gmail.com>
2019-12-12 21:42:21 -0300

    :sparkles: feat: Plans Management finished

--- gympoint-api/src/routes.js
+++ gympoint-api/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
-import PlansController from './app/controllers/PlansController';
+import PlansController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,11 +14,19 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+// STUDENS
+
+routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 routes.put('/user', UserController.update);
 
+// PLANS
+
 routes.post('/plans', PlansController.store);
 routes.get('/plans', PlansController.index);
+routes.get('/plans/:plan_id', PlansController.show);
+routes.put('/plans/:plan_id', PlansController.update);
+routes.delete('/plans/:plan_id', PlansController.delete);
 
 export default routes;
