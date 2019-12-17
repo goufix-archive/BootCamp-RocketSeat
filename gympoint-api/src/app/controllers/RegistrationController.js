247ee43d0bc0f8f35b00d8a0c1c74ef9b72a0ceb
TiagoBehenck <tiago.behenck.santos@gmail.com>
2019-12-17 19:45:45 -0300

    :heavy_plus_sign: Commit for update

--- gympoint-api/src/app/controllers/RegistrationController.js
+++ gympoint-api/src/app/controllers/RegistrationController.js
@@ -2,6 +2,20 @@ class RegistrationController {
   async index(req, res) {
     return res.json({});
   }
+
+  async store(req, res) {
+    const { start_date, plan_id, student_id } = req.body;
+
+    return res.json({ startDate, plan_id });
+  }
+
+  async update(req, res) {
+    return res.json({});
+  }
+
+  async delete(req, res) {
+    return res.json({});
+  }
 }
 
 export default new RegistrationController();
