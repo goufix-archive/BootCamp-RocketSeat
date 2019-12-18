2b6e3cf10a0df45e674a083c123d91ffb0999d0e
TiagoBehenck <tiago.behenck.santos@gmail.com>
2019-12-17 21:59:49 -0300

    :bug: Bug in Registration with format date

--- gympoint-api/src/config/mail.js
+++ gympoint-api/src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: '2525',
   securte: false,
   auth: {
-    user: '4004f535a88e5d',
-    pass: '53779d04e5ab55',
+    user: '',
+    pass: '',
   },
   default: {
     from: 'Equipe GymPoint <noreply@gympoint.com>',
