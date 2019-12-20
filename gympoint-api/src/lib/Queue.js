e8ec21f67400adcad7e4bd0b7e2585916d632fc6
Tiago Santos <tiago.behenck.santos@gmail.com>
2019-12-20 13:33:32 -0300

    :white_check_mark: Template email HelpOrder

--- gympoint-api/src/lib/Queue.js
+++ gympoint-api/src/lib/Queue.js
@@ -32,7 +32,8 @@ class Queue {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
 
-      bee.on('failed', this.handleFailure).process(handle);
+      bee.process(handle);
+      // bee.on('failed', this.handleFailure).process(handle);
     });
   }
 
