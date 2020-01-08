84f560b32c7bace6e3c95f48bb88a076ef2007a5
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-08 14:59:20 -0300

    :sparkles: Styling Home Page

---
+++ modulo07/src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img src="" alt="Tênis" />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
