6f05f25159042a510f8021ecaeec307180903412
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-15 10:28:43 -0300

    :recycle: Restart de challange =)

--- gobarber-api/src/app/controllers/SessionController.js
+++ gobarber-api/src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
@@ -20,7 +21,12 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+      ],
+    });
 
     // Usuário não existe
     if (!user) {
@@ -31,13 +37,16 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     // MD5 Hash gobarberrocketseatnodejs2
     return res.json({
       user: {
         id,
         name,
+        email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
---
+++ gobarber-web/src/config/ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from 'reactotron-react-js';
+
+if (__DEV__) {
+  const tron = Reactotron.configure()
+    .useReactNative()
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
