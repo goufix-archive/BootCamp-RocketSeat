91c5453afe63447951be989829efeb717b6d6fd7
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-20 09:40:45 -0300

    :sparkles: Validations

--- gobarber-web/.eslintrc.js
+++ gobarber-web/.eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: true,
   },
   parser: 'babel-eslint',
   parserOptions: {
@@ -16,7 +18,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'jsx-a11y', 'import'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
@@ -24,5 +26,12 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'react/static-property-placement': ['off', 'property assignment'],
     'react/static-property-placement': ['error', 'static public field'],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
   },
 };
