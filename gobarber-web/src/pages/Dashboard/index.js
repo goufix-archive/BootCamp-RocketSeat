00df2bab3fc9afbee00e46f7cd7eed33c82c20c3
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-21 09:05:41 -0300

    :smoking: Authenticated requests

--- gobarber-web/src/pages/Dashboard/index.js
+++ gobarber-web/src/pages/Dashboard/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
+
+api.get('appointments');
 
 export default function Dashboard() {
   return <h1>Dashboard</h1>;
