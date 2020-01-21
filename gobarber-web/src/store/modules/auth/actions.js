850d8d431f75b05c566fd68d8138278fdb679c62
TiagoBehenck <tiago.behenck.santos@gmail.com>
2020-01-20 23:19:24 -0300

    :white_check_mark: =)

--- gobarber-web/src/store/modules/auth/actions.js
+++ gobarber-web/src/store/modules/auth/actions.js
@@ -1,6 +1,6 @@
 export function signInRequest(email, password) {
   return {
-    type: '@auth/SIGN_IN_RESQUEST',
+    type: '@auth/SIGN_IN_REQUEST',
     payload: { email, password },
   };
 }
@@ -9,6 +9,13 @@ export function signInSuccess(token, user) {
   return {
     type: '@auth/SIGN_IN_SUCCESS',
     payload: { token, user },
+  };
+}
+
+export function signUpRequest(name, email, password) {
+  return {
+    type: '@auth/SIGN_UP_REQUEST',
+    payload: { name, email, password },
   };
 }
 
