6d6985631104d4cd70e7d792783a81a8249c4c70
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-22 09:25:30 -0300

    :trident: Browsin between days

--- gobarber-web/src/pages/Dashboard/index.js
+++ gobarber-web/src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import api from '~/services/api';
 
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
@@ -6,26 +8,41 @@ import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong></strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
 
       <ul>
         <Time past>
-          <strong></strong>
-          <span></span>
+          <strong>8:00</strong>
+          <span>Tiago</span>
         </Time>
         <Time available>
-          <strong></strong>
-          <span></span>
+          <strong>9:00</strong>
+          <span>Em aberto</span>
         </Time>
       </ul>
     </Container>
