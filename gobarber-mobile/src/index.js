1f08e8037894baa1c58adc757637560bbbe8fb78
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-27 21:51:14 -0300

    :tent: Let's bora

--- gobarber-mobile/src/index.js
+++ gobarber-mobile/src/index.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
+import { store, persistor } from './store';
 import Routes from './routes';
 
-export default class index extends Component {
-  render() {
-    return <Routes />;
-  }
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
 }
