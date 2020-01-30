261a95f7cf9f6a4faf1e8814c89e38670024fbbd
Tiago Santos <tiago.behenck.santos@gmail.com>
2020-01-30 11:32:36 -0300

    :rage3: Controllers

--- fastfeet-api/src/app/controllers/RecipientController.js
+++ fastfeet-api/src/app/controllers/RecipientController.js
@@ -1,7 +1,28 @@
 import * as Yup from 'yup';
-import Recipients from '../models/Recipient';
+
+import Recipient from '../models/Recipient';
 
 class RecipientsController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const recipients = await Recipient.findAll({
+      attributes: [
+        'name',
+        'street',
+        'number',
+        'complement',
+        'state',
+        'city',
+        'postal_code',
+      ],
+      limit: 20,
+      offset: (page - 1) * 20,
+    });
+
+    return res.json(recipients);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -19,8 +40,47 @@ class RecipientsController {
       return res.status(400).json({ error: 'Validation Failed' });
     }
 
-    const recipient = await Recipients.create(req.body);
+    const recipient = await Recipient.create(req.body);
     return res.json(recipient);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().notRequired(),
+      complement: Yup.string().notRequired(),
+      state: Yup.string()
+        .required()
+        .max(2),
+      city: Yup.string().required(),
+      postal_code: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed' });
+    }
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(401).json({ error: 'Recipient does not exists' });
+    }
+
+    await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      postal_code,
+    });
   }
 }
 
